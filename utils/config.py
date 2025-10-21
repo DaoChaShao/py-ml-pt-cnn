6a374e98adbe8f199669199035c48166d02e1d32
DaoChaShao <27882272@qq.com>
2025-10-21 20:19:46 +0800

    feat: add dataset paths and fashion class mapping to config.py

--- utils/config.py
+++ utils/config.py
@@ -15,6 +15,23 @@ BASE_DIRECTORY = Path(__file__).resolve().parent.parent
 EXAMPLE_IMG_PATH = BASE_DIRECTORY / "data/Nemo.png"
 # Model save path
 MODEL_SAVE_PATH = BASE_DIRECTORY / "models/model.pth"
+# Train and test dataset path
+TRAIN_DATASET_PATH = BASE_DIRECTORY / "data/fashion-mnist_train.csv"
+TEST_DATASET_PATH = BASE_DIRECTORY / "data/fashion-mnist_test.csv"
+
+# Data map
+FASHION_CLASSES = {
+    0: "T恤/上衣 (T-shirt/top)",
+    1: "裤子 (Trouser)",
+    2: "套头衫 (Pullover)",
+    3: "连衣裙 (Dress)",
+    4: "外套 (Coat)",
+    5: "凉鞋 (Sandal)",
+    6: "衬衫 (Shirt)",
+    7: "运动鞋 (Sneaker)",
+    8: "包 (Bag)",
+    9: "短靴 (Ankle boot)"
+}
 
 # Data processing parameters
 RANDOM_STATE: int = 27
@@ -31,5 +48,5 @@ BATCHES: int = 32
 HIDDEN_UNITS: int = 128
 ALPHA: float = 0.01
 ALPHA4REDUCTION: float = 0.3
-EPOCHS: int = 300
+EPOCHS: int = 20
 ACCELERATOR: str = "cpu"
